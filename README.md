94843e3f1d65ba01b369714d625d07ca0fa7ab65
Sanakoev Anton <totum42@gmail.com>
2017-10-20 12:54:08 +0300

    1. Html fix 2. Any word lenght

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-Word ladder shows shortest by-1-letter-morph-path between 2 words of the same length.
+Word ladder shows shortest by-1-letter-morph path between 2 words of the same length.
 
 How to use:
 1. composer dump-autoload
--- web/index.php
+++ web/index.php
@@ -5,56 +5,58 @@ require '../bootstrap.php';
 use WL\Node;
 use WL\Word;
 
-// keep only 4-letter words in dictionary
-$dictionary = new WL\Dictionary();
-$dictionary->filterWords(4);
-
-$queue = [];
 $startWord = isset($_GET['startWord']) ? $_GET['startWord'] : '';
 $endWord = isset($_GET['endWord']) ? $_GET['endWord'] : '';
+$queue = [];
 $usedWords = [];
 $paths = [];
 $solutions = [];
 
-if ($dictionary->containsWord($startWord) && $dictionary->containsWord($endWord)) {
-    $startNode = new Node($startWord);
+if (mb_strlen($startWord) == mb_strlen($endWord)) {
+    // keep only words with the same length as start and end words in dictionary
+    $dictionary = new WL\Dictionary();
+    $dictionary->filterWords(mb_strlen($startWord));
 
-    array_push($queue, $startNode);
-    array_push($usedWords, $startNode->value);
+    if ($dictionary->containsWord($startWord) && $dictionary->containsWord($endWord)) {
+        $startNode = new Node($startWord);
 
-    while (count($queue) > 0) {
-        $currentNode = array_shift($queue);
+        array_push($queue, $startNode);
+        array_push($usedWords, $startNode->value);
 
-        if ($currentNode->value == $endWord) {
-            array_push($paths, $currentNode);
-            break; // use "continue", if you want multiple solutions
+        while (count($queue) > 0) {
+            $currentNode = array_shift($queue);
+
+            if ($currentNode->value == $endWord) {
+                array_push($paths, $currentNode);
+                break; // use "continue", if you want multiple solutions
+            }
+
+            // find words, that can be received by 1-letter-morph from current word
+            $morphlings = Word::getMorphlings($currentNode->value, $usedWords, $dictionary);
+
+            // add morphlings to the queue
+            foreach ($morphlings as $morphling) {
+                $nextNode = new Node($morphling);
+                $nextNode->parent = $currentNode;
+
+                array_push($queue, $nextNode);
+            }
         }
 
-        // find words, that can be received by 1-letter-morph from current word
-        $morphlings = Word::getMorphlings($currentNode->value, $usedWords, $dictionary);
+        // every solution must ascend to the first word and generate full path
+        foreach ($paths as $path) {
+            $solution = [];
+            array_push($solution, $path->value);
+            $parentNode = $path->parent;
 
-        // add morphlings to the queue
-        foreach ($morphlings as $morphling) {
-            $nextNode = new Node($morphling);
-            $nextNode->parent = $currentNode;
+            while ($parentNode != null) {
+                array_push($solution, $parentNode->value);
+                $parentNode = $parentNode->parent;
+            }
 
-            array_push($queue, $nextNode);
+            array_push($solutions, array_reverse($solution));
         }
-    }
-
-    // every solution must ascend to the first word and generate full path
-    foreach ($paths as $path) {
-        $solution = [];
-        array_push($solution, $path->value);
-        $parentNode = $path->parent;
-
-        while ($parentNode != null) {
-            array_push($solution, $parentNode->value);
-            $parentNode = $parentNode->parent;
-        }
-
-        array_push($solutions, array_reverse($solution));
     }
 }
 
-include 'form.phtml';
+include 'index.phtml';
